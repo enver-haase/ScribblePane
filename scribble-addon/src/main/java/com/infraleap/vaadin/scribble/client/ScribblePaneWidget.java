99b57f6ae07f9fa3efdccc9a11eb2fc9e5c5cd34
Enver Haase <enver.haase@infraleap.com>
2017-05-23 18:54:12 +0200

    Make scribbling work, and add CSS.

--- scribble-addon/src/main/java/com/infraleap/vaadin/scribble/client/ScribblePaneWidget.java
+++ scribble-addon/src/main/java/com/infraleap/vaadin/scribble/client/ScribblePaneWidget.java
@@ -2,28 +2,57 @@ package com.infraleap.vaadin.scribble.client;
 
 import com.google.gwt.canvas.client.Canvas;
 import com.google.gwt.canvas.dom.client.Context2d;
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.SimplePanel;
 
-// Extend any GWT Widget
 public class ScribblePaneWidget extends SimplePanel {
 
-    public ScribblePaneWidget() {
+//    private static native void log(String text) /*-{
+//        console.log(text);
+//    }-*/;
 
-       Canvas canvas = Canvas.createIfSupported();
-       if (canvas != null){
-    	   Context2d context = canvas.getContext2d();
-    	   context.beginPath();
-    	   context.moveTo(25,  0);
-    	   context.lineTo(0, 20);
-    	   context.lineTo(25, 40);
-    	   context.lineTo(25, 0);
-    	   context.fill();
-    	   context.closePath();
-       }
-       add(canvas);
-       
-       //add(new Label("Hallo"));
+    private class Point {
+        private final int x, y;
+
+        private Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+//        @Override
+//        public String toString() {
+//            return "(x: " + x + ", y: " + y + ")";
+//        }
+    }
+
+    private Point lastPoint = new Point(0, 0);
+    private boolean mouseDown = false;
+
+    public ScribblePaneWidget() {
+        setStyleName("scribblepane");
+
+        Canvas canvas = Canvas.createIfSupported();
+        if (canvas != null) {
+            canvas.addMouseDownHandler(e -> {
+                mouseDown = true;
+                lastPoint = new Point(e.getX(), e.getY());
+//                log("MouseDown: Point is " + lastPoint);
+            });
+            canvas.addMouseOutHandler(e -> mouseDown = false);
+            canvas.addMouseUpHandler(e -> mouseDown = false);
+            canvas.addMouseMoveHandler(e -> {
+                if (mouseDown) {
+                    Context2d context = canvas.getContext2d();
+                    context.beginPath();
+                    context.moveTo(lastPoint.x, lastPoint.y);
+                    context.lineTo(e.getX(), e.getY());
+                    context.stroke();
+                    context.closePath();
+                    lastPoint = new Point(e.getX(), e.getY());
+//                    log("MouseMove: Point is " + lastPoint);
+                }
+            });
+        }
+        add(canvas);
 
         // State is set to widget in ScribblePaneConnector
     }
